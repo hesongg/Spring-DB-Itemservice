c50081a9ec60e8a0789444003704301792b29e2b
hesung <hsjo6864@gmail.com>
2022-08-26 15:22:05 +0900

    2022.08.26 Spring ItemService DB Project - JPA 적용 - JPA 리포지토리 적용 후 테스트

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,17 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+//@Table(name = "item") //객체 명과 테이블 명이 같은면 생략 가능
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
